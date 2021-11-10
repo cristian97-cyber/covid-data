5e1b20e5ffbab6aeaf08a9d9713d6fdf2c05ef35
cristian97-cyber <cristianesposito961@gmail.com>
2021-11-10 18:52:34 +0100

    Actualized .gitignore file

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-const COVID_API_URL = "https://covid-api.mmediagroup.fr/v1/";
-const GEO_API_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
-
-const MS_PER_HOUR = 3600000;
-
-export { COVID_API_URL, GEO_API_URL, MS_PER_HOUR };
